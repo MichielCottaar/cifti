d5a06041ada3f24bc3505e9ed04def5b10891f2d
Michiel Cottaar <MichielCottaar@gmail.com>
2017-01-20 14:31:35 +0000

    version 0.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='cifti',
-      version='0.0',
-      description='Creates a simplified interface to handle CIFTI files',
+      version='0.1',
+      description='Interface to handle CIFTI files',
       author='Michiel Cottaar',
       author_email='Michiel.Cottaar@ndcn.ox.ac.uk',
-      url='https://git.fmrib.ox.ac.uk/ndcn0236/gyralcoord',
+      url='https://git.fmrib.ox.ac.uk/ndcn0236/cifti',
       packages=['cifti', 'cifti.tests'],
       install_requires=['numpy', 'nibabel', 'six'],
       )
